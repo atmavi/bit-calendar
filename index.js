bb7a031a7e3baa589406621ec40ded8187145597
atmavi <atmavi0001@gmail.com>
2020-03-25 15:29:02 +0800

    WIP-- header year and month

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-let $= el => document.querySelectorAll(el);
-let days= $('.day');
+let $all= el => document.querySelectorAll(el);
+let $= el => document.querySelector(el);
+
+
+//FORMAT MONTH NAME
+function monthName(date) {
+    let mOptions = { month: 'long'};
+    return new Intl.DateTimeFormat('en-US', mOptions).format(date);
+}
+
+//header
+let headerYear= $('.header__year');
+headerYear.textContent= new Date().getFullYear();
+
+let headerMonth= $('.header__month');
+headerMonth.textContent= monthName(new Date);
+
+let days= $all('.day');
 
 //SET THE DAYS OF THE MONTH (NUMBERS)
 days.forEach((day, index)=> {
